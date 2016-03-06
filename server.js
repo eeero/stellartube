1b8ee5c30ebbb015361b92e64289c1a94ac0ec45
Eero Rautsi <Eero@Eeros-MacBook-Air.local>
2016-03-07 00:11:07 +0200

    added production server

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+});
+
+app.listen(PORT, function() {
+  console.log("Server is up and running on port: " + PORT)
+});
